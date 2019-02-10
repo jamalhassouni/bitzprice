fa556c7b3510c6dff533cf15333dd9fada5a26b9
Jamal Hassouni <jamalhassouni13@gmail.com>
2019-02-10 01:23:47 +0100

    refactor: Fetch Data from Api

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 import React from "react";
+import fetch from "isomorphic-unfetch";
 import Layout from "../components/Layout";
+import Prices from "../components/Prices";
 
-const Home = () => (
+const Home = props => (
   <div>
     <Layout title="Home">
       <div className="hero">
         <h1 className="title">Welcome to Bitzprice!</h1>
-        <p className="description">Application to view Bitcoin prices</p>
+        <p className="description">Check current Bitcoin rates</p>
+        <Prices bpi={props.bpi} />
       </div>
     </Layout>
-
-    <style jsx>{``}</style>
   </div>
 );
+
+Home.getInitialProps = async () => {
+  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const data = await res.json();
+  return {
+    bpi: data.bpi
+  };
+};
 
 export default Home;
